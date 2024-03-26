f1a30ad61fdd3884d9ec5c952e7f4b5ed3f55bd5
MichalAmir <amichal9960@gmail.com>
2024-03-26 12:45:28 +0200

    add controller

---
+++ workers/src/worker/worker.dto.ts
@@ -0,0 +1,15 @@
+// user.dto.ts
+export class UserDto {
+    readonly id: number;
+    readonly firstName: string;
+    readonly lastName: string;
+    readonly username: string;
+    readonly lastLogin: Date;
+    readonly registeredAt: string;
+    readonly mobile: string;
+    readonly status: string;
+    readonly mobileuser: string;
+    readonly dateOfBirth :Date;
+  }
+  
+  
--- workers/src/worker/workers.controller.ts
+++ workers/src/worker/workers.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { WorkersService } from './workers.service';
+import { Worker } from './workers.entity';
 
 @Controller('workers')
-export class WorkersController {}
+export class WorkersController {
+  constructor(private readonly workersService: WorkersService) {}
+
+  @Get()
+  async findAll(): Promise<Worker[]> {
+    return this.workersService.findAll();
+  }
+  
+  @Get(':id')
+  getWorker(@Param('id') id: string) {
+    return this.workersService.getWorker(id);
+  }
+}
