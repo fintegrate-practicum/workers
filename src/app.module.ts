24276d913035dcafcd701ab177bbe276eb4211b5
RikyShafer <r8524409@gmail.com>
2024-03-26 18:24:58 +0200

    Swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose'
+import { AdminModule } from './admin/admin.module';
+import { WorkersModule } from './worker/workers.module';
 @Module({
-  imports: [],
+  imports: [WorkersModule, AdminModule
+    , MongooseModule.forRoot('mongodb+srv://userDBWorkers:1234@workers.iccnfuh.mongodb.net/?retryWrites=true&w=majority&appName=Workers'),],
   controllers: [AppController],
   providers: [AppService],
 })
