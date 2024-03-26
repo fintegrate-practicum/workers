a738e97f6e637b36a4b92d5308a54872df6e7867
Mrs-ID <i6725155@gmail.com>
2024-03-26 16:01:34 +0200

    change app.module.ts -connect to DB compass

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://userDBWorkers:1234@workers.iccnfuh.mongodb.net/?retryWrites=true&w=majority&appName=Workers'),
-  ],
+    // MongooseModule.forRoot('mongodb+srv://userDBWorkers:1234@workers.iccnfuh.mongodb.net/?retryWrites=true&w=majority&appName=Workers'),
+    MongooseModule.forRoot('mongodb://localhost:27017/project')]
+  ,
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppMomongodule { }
