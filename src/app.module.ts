df0b26d6225912e6285a89c2baf9a7b26f2af105
Rachely-Kremer <rachelkr2580@gmail.com>
2024-03-28 18:51:06 +0200

    transform data middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TransformDataStructure } from '../transformDataStructure/convertData';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TransformDataStructure],
 })
 export class AppModule {}
