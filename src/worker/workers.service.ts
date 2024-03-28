099f6b05fa05cd92d12debb4049385272454fad2
AvigailH <avigail054855@gmail.com>
2024-03-28 18:40:08 +0200

    update workers.service

--- src/worker/workers.service.ts
+++ src/worker/workers.service.ts
@@ -1,33 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Worker } from './worker.entity';
+import { Employee } from '../schemes/Employee.entity';
 
 @Injectable()
 export class WorkersService {
-  constructor(@InjectModel('Worker') private readonly workerModel: Model<Worker>) {}
+  constructor(@InjectModel('User') private readonly _EmployeeModel: Model<Employee>) {}
 
-  async createWorker(worker: Worker): Promise<Worker> {
-    const newWorker = new this.workerModel(worker);
-    return await newWorker.save();
+  async createEmployee(worker: Employee): Promise<Employee> {
+    const newEmployee = new this._EmployeeModel(Employee);
+    return await newEmployee.save();
   }
 
-  async findAll(page = 1, limit = 10): Promise<{ workers: Worker[], total: number }> {
+  async findAll(page = 1, limit = 10): Promise<{ Employee: Employee[], total: number }> {
     const skip = (page - 1) * limit;
-    const workers = await this.workerModel.find().skip(skip).limit(limit).exec();
-    const total = await this.workerModel.countDocuments();
-    return { workers, total };
+    const Employee = await this._EmployeeModel.find().skip(skip).limit(limit).exec();
+    const total = await this._EmployeeModel.countDocuments();
+    return { Employee, total };
   }
 
-  async getWorker(id: string): Promise<Worker> {
-    return await this.workerModel.findById(id).exec();
+  async getEmployee(id: string): Promise<Employee> {
+    return await this._EmployeeModel.findById(id).exec();
   }
 
-  async updateWorker(id: string, worker: Worker): Promise<Worker> {
-    return await this.workerModel.findByIdAndUpdate(id, worker, { new: true }).exec();
+  async updateEmployee(id: string, Employee: Employee): Promise<Employee> {
+    return await this._EmployeeModel.findByIdAndUpdate(id, Employee, { new: true }).exec();
   }
 
-  async deleteWorker(id: string): Promise<Worker> {
-    return await this.workerModel.findByIdAndDelete(id).exec();
+  async deleteEmployee(id: string): Promise<Employee> {
+    return await this._EmployeeModel.findByIdAndDelete(id).exec();
   }
 }
