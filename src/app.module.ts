f49b31bd588f9145f6e45d6d1129116ff042cb82
shulamitRoz <shulamit6670@gmail.com>
2024-03-28 18:40:02 +0200

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose'
+import { MongooseModule } from '@nestjs/mongoose';
 import { AdminModule } from './admin/admin.module';
 import { WorkersModule } from './worker/workers.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 @Module({
-  imports: [WorkersModule, AdminModule
-    , MongooseModule.forRoot('mongodb+srv://userDBWorkers:1234@workers.iccnfuh.mongodb.net/?retryWrites=true&w=majority&appName=Workers'),],
+  imports: [
+    ConfigModule.forRoot(), // Load environment variables from .env file
+    WorkersModule,
+    AdminModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule], // Inject ConfigModule to use ConfigService
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('mongodb+srv://userDBWorkers:1234@workers.iccnfuh.mongodb.net/?retryWrites=true&w=majority&appName=Workers'), // Get MongoDB URI from environment variable
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
