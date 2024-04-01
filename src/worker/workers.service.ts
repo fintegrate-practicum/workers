e157d26f2c68098eccba7d5f5a3761e7355868c7
Mrs-ID <i6725155@gmail.com>
2024-04-01 14:57:24 +0300

    rename schemas folder and imports

--- src/worker/workers.service.ts
+++ src/worker/workers.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Employee } from '../schemes/employee.entity'
+import { Employee } from '../schemas/employee.entity'
 @Injectable()
 export class WorkersService {
   constructor(@InjectModel('Employee') private readonly employeeModel: Model<Employee>) { }
