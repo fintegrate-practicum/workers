56762b8a68748980ace787ed72b1aee21909d565
shulamitRoz <shulamit6670@gmail.com>
2024-04-02 09:26:00 +0300

    update moduls app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AdminModule } from './admin/admin.module';
 import { WorkersModule } from './worker/workers.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(), // Load environment variables from .env file
+    ConfigModule.forRoot(), // Load environment variables
     WorkersModule,
     AdminModule,
     MongooseModule.forRootAsync({
       imports: [ConfigModule], // Inject ConfigModule to use ConfigService
-      useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('mongodb+srv://userDBWorkers:1234@workers.iccnfuh.mongodb.net/?retryWrites=true&w=majority&appName=Workers'), // Get MongoDB URI from environment variable
+      useFactory: async (config) => ({
+        uri: config.get('DATABASE_URI'), // Use DATABASE_URI from environment variable
       }),
       inject: [ConfigService],
     }),
