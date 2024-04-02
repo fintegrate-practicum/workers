313a9048f6dd4b6cd66ce86dad5ef83c68643635
AvigailH <avigail054855@gmail.com>
2024-04-02 11:32:23 +0300

    add unit test

--- src/worker/workers.service.spec.ts
+++ src/worker/workers.service.spec.ts
@@ -1,18 +1,87 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WorkersService } from './workers.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Employee } from './schemas/employee.schema';
+import { Model } from 'mongoose';
 
 describe('WorkersService', () => {
-  let service: WorkersService;
+  let workersService: WorkersService;
+  let model: Model<Employee>;
+
+const mockEmployee = [{
+  code: '1',
+  position: 'deliveryPerson',
+  roleId: 30,
+  createdBy: "Dan",
+  updatedBy: "Yon",
+}];
+
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [WorkersService],
+      providers: [
+        WorkersService,
+        {
+          provide: getModelToken(Employee.name),
+          useValue: {
+            create: jest.fn(),
+            findById: jest.fn(),
+            findByIdAndUpdate: jest.fn(),
+            findByIdAndDelete: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<WorkersService>(WorkersService);
+    workersService = module.get<WorkersService>(WorkersService);
+    model = module.get<Model<Employee>>(getModelToken(Employee.name));
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('createEmployee', () => {
+    it('should create and return an employee', async () => {
+      jest.spyOn(model, 'create').mockResolvedValue(mockEmployee);
+
+      const newEmployee: Employee = {
+        position: 'deliveryPerson',
+        roleId: 30,
+        createdBy: "Dan",
+        updatedBy: "Yon",
+      };
+
+      const result = await workersService.createEmployee(newEmployee);
+      expect(result).toEqual(mockEmployee);
+    });
+  });
+
+  describe('getEmployee', () => {
+    it('should find and return an employee by ID', async () => {
+      jest.spyOn(model, 'findById').mockResolvedValue(mockEmployee);
+
+      const result = await workersService.getEmployee(mockEmployee.code);
+      expect(result).toEqual(mockEmployee);
+    });
+  });
+
+  describe('updateEmployee', () => {
+    it('should update and return an employee by ID', async () => {
+      jest.spyOn(model, 'findByIdAndUpdate').mockResolvedValue(mockEmployee);
+
+      const updatedEmployeeData: Employee = {
+        position: 'Senior Engineer',
+        age: 35,
+      };
+
+      const result = await workersService.updateEmployee(mockEmployee.code, updatedEmployeeData);
+      expect(result).toEqual(mockEmployee);
+    });
+  });
+
+  describe('deleteEmployee', () => {
+    it('should delete and return an employee by ID', async () => {
+      jest.spyOn(model, 'findByIdAndDelete').mockResolvedValue(mockEmployee);
+
+      const result = await workersService.deleteEmployee(mockEmployee.code);
+      expect(result).toEqual(mockEmployee);
+    });
   });
 });
--- src/worker/workers.service.ts
+++ src/worker/workers.service.ts
@@ -5,29 +5,29 @@ import { Employee } from '../schemes/Employee.entity';
 
 @Injectable()
 export class WorkersService {
-  constructor(@InjectModel('User') private readonly _EmployeeModel: Model<Employee>) {}
+  constructor(@InjectModel('Employee') private readonly _employeeModel: Model<Employee>) {}
 
   async createEmployee(worker: Employee): Promise<Employee> {
-    const newEmployee = new this._EmployeeModel(Employee);
+    const newEmployee = new this._employeeModel(worker);
     return await newEmployee.save();
   }
 
-  async findAll(page = 1, limit = 10): Promise<{ Employee: Employee[], total: number }> {
+  async findAll(page = 1, limit = 10): Promise<{ employees: Employee[], total: number }> {
     const skip = (page - 1) * limit;
-    const Employee = await this._EmployeeModel.find().skip(skip).limit(limit).exec();
-    const total = await this._EmployeeModel.countDocuments();
-    return { Employee, total };
+    const employees = await this._employeeModel.find().skip(skip).limit(limit).exec();
+    const total = await this._employeeModel.countDocuments();
+    return { employees, total };
   }
 
   async getEmployee(id: string): Promise<Employee> {
-    return await this._EmployeeModel.findById(id).exec();
+    return await this._employeeModel.findById(id).exec();
   }
 
-  async updateEmployee(id: string, Employee: Employee): Promise<Employee> {
-    return await this._EmployeeModel.findByIdAndUpdate(id, Employee, { new: true }).exec();
+  async updateEmployee(id: string, updatedEmployee: Employee): Promise<Employee> {
+    return await this._employeeModel.findByIdAndUpdate(id, updatedEmployee, { new: true }).exec();
   }
 
   async deleteEmployee(id: string): Promise<Employee> {
-    return await this._EmployeeModel.findByIdAndDelete(id).exec();
+    return await this._employeeModel.findByIdAndDelete(id).exec();
   }
 }
