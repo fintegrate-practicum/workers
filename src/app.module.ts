d1ba3613d2e93bfa12a34c225fadc150f4bf3b0a
RikyShafer <r8524409@gmail.com>
2024-04-02 09:33:51 +0300

    Swagger Module put

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose'
-import { AdminModule } from './admin/admin.module';
 import { WorkersModule } from './worker/workers.module';
+
 @Module({
-  imports: [WorkersModule, AdminModule
-    , MongooseModule.forRoot('mongodb+srv://userDBWorkers:1234@workers.iccnfuh.mongodb.net/?retryWrites=true&w=majority&appName=Workers'),],
+  imports: [WorkersModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,54 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { AdminModule } from './admin/admin.module';
-import { WorkersModule } from './worker/workers.module';
 
+async function initializeSwagger(app) {
+  const config = new DocumentBuilder()
+    .setTitle('Main api example')
+    .setDescription('The main API description')
+    .setVersion('1.0')
+    .addTag('main')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  const config = new DocumentBuilder()
-    .setTitle('Cats example') 
-    .setDescription('The cats API description')  
-    .setVersion('1.0')  
-    .addTag('cats')   
-    .build(); 
-  const document = SwaggerModule.createDocument(app, config); 
-  SwaggerModule.setup('api', app, document); 
 
+  await initializeSwagger(app);
 
-   const adminConfig = new DocumentBuilder()
-   .setTitle('Admin API') 
-   .setDescription('API for Admin operations') 
-   .setVersion('1.0') 
-   .addTag('admin') 
-   .build(); 
- const adminDocument = SwaggerModule.createDocument(app, adminConfig); 
- SwaggerModule.setup('admin/api', app, adminDocument);
-  
- 
-    const workerConfig = new DocumentBuilder()
-    .setTitle('worker API') 
-    .setDescription('API for Worker operations') 
-    .setVersion('1.0')
-    .addTag('worker')
-    .build(); 
-  const workerDocument = SwaggerModule.createDocument(app, workerConfig);
-    SwaggerModule.setup('worker/api', app, workerDocument); 
-   
-
-
-
-
-    await app.listen(3030);
-    console.log(`Application is running on: ${await app.getUrl()}`);
-
+  await app.listen(3030);
+  // console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
-
-
-
-
-
-
