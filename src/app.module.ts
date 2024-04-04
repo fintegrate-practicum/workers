93c43e47ec9ce298bcc6cb47df9778f0e0689ca9
Mrs-ID <i6725155@gmail.com>
2024-04-04 17:55:06 +0300

    update app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
+import { AdminModule } from './admin/admin.module';
+import { WorkersModule } from './worker/workers.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    //MongooseModule.forRoot(process.env.MONGODB_CONNECTION_ATLAS),
-    MongooseModule.forRoot(process.env.MONGODB_CONNECTION_COMPASS),
+    WorkersModule,
+    AdminModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get(process.env.MONGODB_CONNECTION_COMPASS),
+        //uri: config.get(process.env.MONGODB_CONNECTION_ATLAS),
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
