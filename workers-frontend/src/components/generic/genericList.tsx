66ef8da565f6648ef8e77540e39ce07ca2e4db0e
shirashtub <shirashtub@gmail.com>
2024-04-04 17:31:17 +0300

    generic-list

--- workers-frontend/src/components/generic/genericList.tsx
+++ workers-frontend/src/components/generic/genericList.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -10,33 +9,37 @@ import Item from "./item";
 const GenericList = (props: {
     title: string,
     list: Object[],
-    column: string[]
+    column: string[],
+    desing: HTMLElement | null
 }) => {
     const list = props.list;
     const column = props.column;
     return(
         <>
         <h2>{props.title}</h2>
-        <TableContainer>
-            <Table aria-label="collapsible table">
-                <TableHead>
-                <TableRow>
-                    {
-                        column.map((c)=>{
-                            return(<TableCell align="left" sx={{fontWeight: "bold"}}>{c}</TableCell>)
-                        })
-                    }
-                </TableRow>
-                </TableHead>
-                <TableBody>
-                    {
-                        list.map((l) => (
-                            <Item column={column} item={l} />
-                        ))
-                    }
-                </TableBody>
-            </Table>
-        </TableContainer>
+        {
+            props.desing? props.desing:
+            <TableContainer>
+                <Table aria-label="collapsible table">
+                    <TableHead>
+                    <TableRow>
+                        {
+                            column.map((c)=>{
+                                return(<TableCell align="left" sx={{fontWeight: "bold"}}>{c}</TableCell>)
+                            })
+                        }
+                    </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {
+                            list.map((l) => (
+                                <Item column={column} item={l} />
+                            ))
+                        }
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        }
         </>
     )
 }
