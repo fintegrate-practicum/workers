c399e430cb4d63f7335de0f3eeab6d523cd8f09e
ChaniRoz <m05276539232@GMAIL.COM>
2024-05-17 00:03:15 +0300

    create taskSlice adding api-call for create task and update employeeSlice

--- workers-backend/src/app.service.ts
+++ workers-backend/src/app.service.ts
@@ -1,9 +1,8 @@
-import { Injectable,Logger } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-
-  private readonly logger = new Logger(AppService.name)
+  private readonly logger = new Logger(AppService.name);
 
   getHello(): string {
     return 'Hello World!';
