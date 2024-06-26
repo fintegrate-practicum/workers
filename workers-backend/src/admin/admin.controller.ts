709c935744c2d0dd18eedadcf906479735842704
Rachely-Kremer <rachelkr2580@gmail.com>
2024-06-26 14:45:56 +0300

    add funstions  : getUserById,  getUsersByBusinessId

--- workers-backend/src/admin/admin.controller.ts
+++ workers-backend/src/admin/admin.controller.ts
@@ -1,12 +1,22 @@
-import { Controller } from '@nestjs/common';
-// import { Controller, Get, Param } from '@nestjs/common';
-
+import { Controller, Get, Param } from '@nestjs/common';
 import { AdminService } from './services/admin.service';
+import {User} from '../schemas/user.entity'
 // import { Admin } from './admin.entity';
+
 
 @Controller('admin')
 export class AdminController {
-  constructor(private readonly adminService: AdminService) {}
+  constructor(private readonly adminService: AdminService) { }
+
+  @Get('user/:id')
+  async getUserById(@Param('id') id: string) :Promise<User>{
+    return this.adminService.getUserById(id);
+  }
+
+  @Get('business/:businessId/users')
+  async getUsersByBusinessId(@Param('businessId') businessId: string) :Promise<User[]>{
+    return this.adminService.getUsersByBusinessId(businessId);
+  }
 
   // @Get()
   // async findAll(): Promise<Admin[]> {
