9d0f8e40fc53567fc73010c3512ef74fe373ff2c
RikyShafer <r8524409@gmail.com>
2024-06-27 15:32:53 +0300

    set auto0_id

--- workers-backend/src/user/controllers/users.controller.ts
+++ workers-backend/src/user/controllers/users.controller.ts
@@ -1,3 +1,76 @@
+// import {
+//   Controller,
+//   Get,
+//   Param,
+//   Body,
+//   Post,
+//   BadRequestException,
+//   Put,
+//   UseGuards,
+//   Request,
+// } from '@nestjs/common';
+// import { UserService } from '../services/users.service';
+// import { ApiTags } from '@nestjs/swagger'; // Import Swagger decorators
+// import { Logger } from '@nestjs/common';
+// import { CreateUserDto } from 'src/dto/createUser.dto';
+// import { UpdateUserDto } from 'src/dto/updateUser.dto';
+// import { User } from 'src/schemas/user.entity';
+// import { AuthGuard } from '@nestjs/passport';
+// @ApiTags('User')
+// @Controller('user')
+// export class UsersController {
+//   private readonly logger = new Logger(UsersController.name);
+
+//   constructor(private readonly _userService: UserService) {}
+//   @Get(':id')
+//   getWorker(@Param('id') auth0_user_id: string) {
+//     return this._userService.findOneByUserId(auth0_user_id);
+//   }
+//   @Get('jwt')
+//   @UseGuards(AuthGuard('jwt'))
+//   async checkAndAddUser(@Request() req): Promise<string> {
+//     const auth0_user_id = req.user.id;
+
+//     // Check if the user already exists in the database
+//     const existingUser = await this._userService.getUser(auth0_user_id);
+//     if (existingUser) {
+//       return `User with id ${auth0_user_id} already exists.`;
+//     }
+
+//     // If the user doesn't exist, add them to the database
+//     const newUser: CreateUserDto = {
+//       auth0_user_id, // Ensure your DTO includes the necessary fields
+//       userName: req.user.name, // Example: assuming the user has a 'name' property
+//       userEmail: req.user.email, // Example: assuming the user has an 'email' property
+//     };
+
+//     try {
+//       await this._userService.createUser(newUser);
+//       return `User with id ${auth0_user_id} added successfully.`;
+//     } catch (error) {
+//       throw new BadRequestException('Failed to create user');
+//     }
+//   }
+//   @Post('')
+//   async createUser(@Body() user: CreateUserDto) {
+//     try {
+//       return this._userService.createUser(user);
+//     } catch (error) {
+//       throw new BadRequestException(error.message);
+//     }
+//   }
+
+//   @Put(':id')
+//   async updateUser(@Param('id') id: string, @Body() user: User) {
+//     try {
+//       return this._userService.updateUser(id, user);
+//     } catch (error) {
+//       throw new BadRequestException(error.message);
+//     }
+//   }
+// }
+
+
 import {
   Controller,
   Get,
@@ -6,22 +79,77 @@ import {
   Post,
   BadRequestException,
   Put,
+  UseGuards,
+  Request,
 } from '@nestjs/common';
 import { UserService } from '../services/users.service';
-import { ApiTags } from '@nestjs/swagger'; // Import Swagger decorators
+import { ApiTags } from '@nestjs/swagger';
 import { Logger } from '@nestjs/common';
 import { CreateUserDto } from 'src/dto/createUser.dto';
 import { UpdateUserDto } from 'src/dto/updateUser.dto';
 import { User } from 'src/schemas/user.entity';
+import { AuthGuard } from '@nestjs/passport';
+import { log } from 'console';
+
 @ApiTags('User')
 @Controller('user')
 export class UsersController {
   private readonly logger = new Logger(UsersController.name);
 
-  constructor(private readonly _userService: UserService) {}
+  constructor(private readonly _userService: UserService) { }
+
   @Get(':id')
   getWorker(@Param('id') auth0_user_id: string) {
     return this._userService.findOneByUserId(auth0_user_id);
+  }
+
+  @Put('jwt')
+  @UseGuards(AuthGuard('jwt'))
+  async checkAndAddUser(@Request() req): Promise<string> {
+    const auth0_user_id = req.user.id;
+    const emailFromHeaders = req.headers['Email'];     
+    if (!emailFromHeaders) {
+      throw new BadRequestException('Email not found in headers');
+    }
+    console.log(`User ID: ${auth0_user_id}`);
+    console.log(`User Email: ${emailFromHeaders}`);
+    const existingUser = await this._userService.findOneByUserId(auth0_user_id);
+    if (existingUser) {
+      return `User with id ${auth0_user_id} already exists.`;
+    }
+ const existingUserByEmail = await this._userService.findOneByEmail(emailFromHeaders);
+ 
+ if (existingUserByEmail) {
+   if( !existingUserByEmail.auth0_user_id)
+      existingUserByEmail.auth0_user_id = auth0_user_id;
+   console.log(existingUserByEmail.auth0_user_id);
+   await this._userService.updateUser( existingUserByEmail.id ,existingUserByEmail);
+
+      return `User with email ${emailFromHeaders} already exists and was updated with the new ID ${auth0_user_id}.`;
+ }
+ //במקרה שלא מצאה לא ידענו אם נכון להוסיף  אותו למסד 
+    const newUser: CreateUserDto = {
+      auth0_user_id,
+      userName:  null,
+      userEmail: null,
+      registeredAt: new Date(),
+      lastLogin: new Date(),
+      mobile:  null,
+      status: 'Bachelor', 
+      dateOfBirth: null,
+      address: {
+        city:  null,
+        street: null,
+        num:  null,
+      },
+    };
+
+    try {
+      await this._userService.createUser(newUser);
+      return `User with id ${auth0_user_id} added successfully.`;
+    } catch (error) {
+      throw new BadRequestException('Failed to create user');
+    }
   }
 
   @Post('')
@@ -34,7 +162,7 @@ export class UsersController {
   }
 
   @Put(':id')
-  async updateUser(@Param('id') id: string, @Body() user: User) {
+  async updateUser(@Param('id') id: string, @Body() user: UpdateUserDto) {
     try {
       return this._userService.updateUser(id, user);
     } catch (error) {
--- workers-backend/src/user/services/users.service.ts
+++ workers-backend/src/user/services/users.service.ts
@@ -1,3 +1,73 @@
+// import {
+//   BadRequestException,
+//   HttpException,
+//   HttpStatus,
+//   NotFoundException,
+// } from '@nestjs/common';
+// import { Injectable, Logger } from '@nestjs/common';
+// import { InjectModel } from '@nestjs/mongoose';
+// import { Model, Mongoose } from 'mongoose';
+// import { CreateUserDto } from 'src/dto/createUser.dto';
+// import { UpdateUserDto } from 'src/dto/updateUser.dto';
+// import { Employee } from 'src/schemas/employee.entity';
+// import { User } from 'src/schemas/user.entity';
+
+// @Injectable()
+// export class UserService {
+//   private readonly logger = new Logger(UserService.name);
+
+//   constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
+
+//   async findOneByUserId(userId: string): Promise<User | undefined> {
+//     const user = await this.userModel.findById(userId).exec();
+//     console.log(user);
+
+//     return user;
+//   }
+
+//   async getUser(auth0_user_id: string): Promise<User> {
+//     try {
+//       const user = await this.userModel.findOne({ auth0_user_id }).exec();
+//       if (!user) {
+//         throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+//       }
+//       return user;
+//     } catch (error) {
+//       if (error.status === HttpStatus.NOT_FOUND) {
+//         throw error;
+//       }
+//       throw new HttpException(
+//         'Error fetching user',
+//         HttpStatus.INTERNAL_SERVER_ERROR,
+//       );
+//     }
+//   }
+
+//   async createUser(user: CreateUserDto): Promise<CreateUserDto> {
+//     const newUser = new this.userModel(user);
+//     try {
+//       return await newUser.save();
+//     } catch (error) {
+//       throw new BadRequestException(error.message);
+//     }
+//   }
+
+//   async updateUser(id: string, user: User): Promise<User> {
+//     try {
+//       const updatedUser = await this.userModel.findOneAndUpdate(
+//         { _id: id },
+//         user,
+//         { new: true },
+//       );
+//       if (!updatedUser) {
+//         throw new NotFoundException(`User with ID ${id} not found`);
+//       }
+//       return updatedUser;
+//     } catch (error) {
+//       throw new BadRequestException(error.message);
+//     }
+//   }
+// }
 import {
   BadRequestException,
   HttpException,
@@ -6,10 +76,9 @@ import {
 } from '@nestjs/common';
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model, Mongoose } from 'mongoose';
+import { Model } from 'mongoose';
 import { CreateUserDto } from 'src/dto/createUser.dto';
 import { UpdateUserDto } from 'src/dto/updateUser.dto';
-import { Employee } from 'src/schemas/employee.entity';
 import { User } from 'src/schemas/user.entity';
 
 @Injectable()
@@ -19,31 +88,26 @@ export class UserService {
   constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
 
   async findOneByUserId(userId: string): Promise<User | undefined> {
-    const user = await this.userModel.findById(userId).exec();
-    console.log(user);
-
-    return user;
-  }
-
-  async getUser(auth0_user_id: string): Promise<User> {
     try {
-      const user = await this.userModel.findOne({ auth0_user_id }).exec();
-      if (!user) {
-        throw new HttpException('User not found', HttpStatus.NOT_FOUND);
-      }
+      const user = await this.userModel.findOne({ auth0_user_id: userId }).exec();
       return user;
     } catch (error) {
-      if (error.status === HttpStatus.NOT_FOUND) {
-        throw error;
-      }
-      throw new HttpException(
-        'Error fetching user',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
+      this.logger.error('Failed to find user', error.stack);
+      throw new HttpException('Error fetching user', HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
-
-  async createUser(user: CreateUserDto): Promise<CreateUserDto> {
+  async findOneByEmail(email: string): Promise<User | undefined> {
+    try {
+      const user = await this.userModel.findOne({ userEmail: email }).exec();
+      console.log("fsdafs", user);
+      
+      return user;
+    } catch (error) {
+      this.logger.error('Failed to find user by email', error.stack);
+      throw new HttpException('Error fetching user', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+  async createUser(user: CreateUserDto): Promise<User> {
     const newUser = new this.userModel(user);
     try {
       return await newUser.save();
@@ -52,13 +116,13 @@ export class UserService {
     }
   }
 
-  async updateUser(id: string, user: User): Promise<User> {
+  async updateUser(id: string, user: UpdateUserDto): Promise<User> {
     try {
       const updatedUser = await this.userModel.findOneAndUpdate(
         { _id: id },
         user,
         { new: true },
-      );
+      ).exec();
       if (!updatedUser) {
         throw new NotFoundException(`User with ID ${id} not found`);
       }
