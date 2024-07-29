b48b5f6c3e6ce64218f634c09b6f1fabe9016ad8
noah-cohen <n0548548106@gmail.com>
2024-07-29 10:17:23 +0200

    upgate controller

--- src/worker/controllers/workers.controller.spec.ts
+++ src/worker/controllers/workers.controller.spec.ts
@@ -1,137 +1,107 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { HttpException, HttpStatus } from '@nestjs/common';
+import {Controller,Get,Param,UseInterceptors,Query,Body,Post,ValidationPipe,BadRequestException,UseGuards,Put,NotFoundException,}from '@nestjs/common';
 import { WorkersService } from '../services/workers.service';
-import { WorkersController } from '../controllers/workers.controller';
 import { Employee } from '../../schemas/employee.entity';
-import { Types } from 'mongoose';
-import { Role } from '../../schemas/employeeRole.entity';
-import { DESTRUCTION } from 'dns';
 import { workerValidationsSchema } from '../validations/worker.validations.schema';
-import { RoleValidationSchema } from '../validations/worker.roleValidation.schema';
+import { Logger } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { TransformDataStructure } from '../../transformDataStructure/convertData';
+import { Request, Response } from 'express';
+import { ApiTags, ApiOperation, ApiBody, ApiBearerAuth } from '@nestjs/swagger';
 
-describe('WorkersController', () => {
-  let controller: WorkersController;
-  let service: WorkersService;
+@ApiTags('Workers')
+@Controller('workers')
+export class WorkersController {
+  private readonly logger = new Logger(WorkersController.name);
 
-  const mockEmployee: Employee = {
-    businessId: '123',
-    code: 'EMP001',
-    createdBy: 'admin',
-    updatedBy: 'admin',
-    role: {
-      type: 'aa',
-      activate: false,
-      description: 'developer',
-    } as unknown as Role,
-  } as unknown as Employee;
+  constructor(private readonly workersService: WorkersService) { }
+  @ApiBearerAuth()
+  @ApiTags('workers')
+  @UseInterceptors(TransformDataStructure)
+  @Get()
+  @UseGuards(AuthGuard('jwt'))
+  async findAll(@Query('businessId') businessId: string): Promise<Employee[]> {
+    return this.workersService.findAll(businessId);
+  }
 
-  const mockWorkersService = {
-    activateEmployee: jest.fn(),
-  };
+  @Get('employee/:id')
+  @ApiOperation({ summary: 'Activate an employee' })
+  @Post(':id/activate')
+  async activateEmployee(@Param('id') id: string): Promise<Employee> {
+    const employee = await this.workersService.activateEmployee(id);
+    if (!employee) {
+      throw new NotFoundException('employee not found');
+    }
+    return employee;
+  }
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [WorkersController],
-      providers: [
-        {
-          provide: WorkersService,
-          useValue: mockWorkersService,
+  @Get(':id')
+  getWorker(@Param('id') id: string) {
+    const employee = this.workersService.getEmployeeByUserId(id);
+    if (!employee)
+      throw new NotFoundException('employee not found');
+    return employee;
+  }
+
+  @Get('data')
+  @UseGuards(AuthGuard('jwt'))
+  @UseInterceptors(TransformDataStructure)
+  async getData(@Body() req: Request, @Body() res: Response): Promise<void> {
+    res.json({ message: 'Original data' });
+  }
+
+  @Get('company/:companyId')
+  @UseGuards(AuthGuard('jwt'))
+  async get(@Param('companyId') id: string): Promise<Employee[]> {
+    const result = await this.workersService.findAllByBusinessId(id);
+    return result;
+  }
+
+  @ApiOperation({ summary: 'Add a new employee' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        userId: { type: 'string' },
+        businessId: { type: 'string' },
+        code: { type: 'string' },
+        createdBy: { type: 'string' },
+        updateBy: { type: 'string' },
+        nameEmployee: { type: 'string' },
+        role: {
+          type: 'object',
+          properties: {
+            type: { type: 'string' },
+            active: { type: 'boolean' },
+            description: { type: 'string' },
+          },
         },
-      ],
-    }).compile();
+      },
+    },
+  })
+  @Post('')
+  @UseGuards(AuthGuard('jwt'))
+  async create(
+    @Body(
+      new ValidationPipe({
+        exceptionFactory: (errors) => {
+          Logger.log('error validation! ' + errors);
+          return new BadRequestException(errors);
+        },
+      }),
+    )
+    @Body() requestBody: workerValidationsSchema,) {
+    this.logger.log(requestBody);
+    const result = this.workersService.createEmployee(requestBody);
+    this.logger.log('good');
+    return result;
+  }
 
-    controller = module.get<WorkersController>(WorkersController);
-    service = module.get<WorkersService>(WorkersService);
-  });
-
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-
-  describe('activateEmployee', () => {
-    it('should activate an employee successfully', async () => {
-      const activatedEmployee = { ...mockEmployee, active: true };
-      mockWorkersService.activateEmployee.mockResolvedValueOnce(
-        activatedEmployee,
-      );
-
-      const result = await controller.activateEmployee(
-        '60d9c6f3f9b5b61710f0f4f4',
-      );
-
-      expect(result).toEqual(activatedEmployee);
-      // expect(result.active).toBe(true);
-      expect(service.activateEmployee).toHaveBeenCalledWith(
-        '60d9c6f3f9b5b61710f0f4f4',
-      );
-    });
-
-    it('should create a new employee', async () => {
-      // const requestBody: workerValidationsSchema = {
-      //   userId: '60d9c6f3f9b5b61710f0f4f4',
-      //   nameEmployee: 'aa',
-      //   businessId: '123456',
-      //   createdBy: 'John Doe',
-      //   code: 'bb',
-      //   updateBy: 'aa',
-      //   role: new RoleValidationSchema(),
-      // };
-
-      // const createdEmployee = {
-      //   _id: 'someId',
-      //   ...requestBody,
-      // };
-
-      // jest
-      //   .spyOn(mockWorkersService, 'createEmployee')
-      //   .mockResolvedValueOnce(createdEmployee as unknown as Employee);
-      // const result = await controller.create(requestBody);
-
-      // expect(result).toEqual(createdEmployee);
-    });
-    it('should activate an employee successfully', async () => {
-      const activatedEmployee = { ...mockEmployee, active: true };
-      mockWorkersService.activateEmployee.mockResolvedValueOnce(
-        activatedEmployee,
-      );
-
-      const result = await controller.activateEmployee(
-        '60d9c6f3f9b5b61710f0f4f4',
-      );
-
-      expect(result).toEqual(activatedEmployee);
-      // expect(result.active).toBe(true);
-      expect(service.activateEmployee).toHaveBeenCalledWith(
-        '60d9c6f3f9b5b61710f0f4f4',
-      );
-    });
-  });
-
-  it('should handle errors during employee creation', async () => {
-    // const requestBody: workerValidationsSchema = {
-    //   userId: '',
-    //   nameEmployee: '',
-    //   businessId: '',
-    //   code: '',
-    //   createdBy: '',
-    //   role: new RoleValidationSchema(),
-    //   updateBy: '',
-    // };
-
-    // const errorMessage = 'Internal server error';
-
-    // jest
-    //   .spyOn(workersService, 'createEmployee')
-    //   .mockRejectedValueOnce(
-    //     new HttpException(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR),
-    //   );
-
-    // try {
-    //   await controller.create(requestBody);
-    // } catch (error) {
-    //   expect(error).toBeInstanceOf(HttpException);
-    //   expect(error.message).toBe(errorMessage);
-    //   expect(error.status).toBe(HttpStatus.INTERNAL_SERVER_ERROR);
-    // }
-  });
-});
+  @Put(':id')
+  updateUser(@Param('id') id: string, @Body() user: Employee) {
+      const response = this.workersService.updateEmployeeByUserId(id, user);
+      if (!response) {
+        throw new NotFoundException('employee not found');
+      }
+      return response;
+  }
+}
