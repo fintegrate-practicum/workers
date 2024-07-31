9e47a4598bfdecd21b02ced6ed075e0d5a5b3f4e
efchbd1013 <efchbd1013@gmail.com>
2024-08-01 02:13:14 +0300

    Removing unnecessary lines in app

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,6 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RabbitPublisherService } from './rabbit-publisher/rabbit-publisher.service';
 import { WorkersModule } from './worker/module/workers.module';
-import { Employee } from './schemas/employee.entity';
-import { env } from 'process';
 import { AuthzModule } from 'fintegrate-auth';
 import { TasksModule } from './tasks/module/tasks.module';
 import { TransformDataStructure } from './transformDataStructure/convertData';
@@ -21,7 +19,6 @@ import { MessagesModule } from './message/module/messages.module';
     TasksModule,
     WorkersModule,
     MessagesModule,
-    TasksModule,
     MongooseModule.forRootAsync({
       imports: [ConfigModule, WorkersModule, AuthzModule],
       useFactory: async (config: ConfigService) => ({
