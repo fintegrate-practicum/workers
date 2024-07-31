f5ca71e98c023622a887c0fe46405b912a417351
efchbd1013 <efchbd1013@gmail.com>
2024-07-31 23:33:41 +0300

    adding the user.entity file

---
+++ src/schemas/user.entity.ts
@@ -0,0 +1,39 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+@Schema()
+export class User extends Document {
+  @Prop()
+  userName: string;
+
+  @Prop()
+  userEmail: string;
+
+  @Prop()
+  auth0_user_id: string;
+
+  @Prop()
+  registeredAt: Date;
+
+  @Prop()
+  lastLogin: Date;
+
+  @Prop()
+  mobile: string;
+
+  @Prop({ enum: ['Married', 'divorcee', 'widower', 'Bachelor'] })
+  status: string;
+
+  @Prop()
+  dateOfBirth: Date;
+
+  @Prop({ type: Object })
+  address: {
+    city: string;
+    street: string;
+    num: number;
+  };
+  @Prop()
+  phone: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
