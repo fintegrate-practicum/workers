6f3254cd02ac4e586839530017dcfb731cbe04d9
nechama lerner <nechamy5834@gmail.com>
2024-08-01 01:05:05 +0300

    revert 6 commits

---
+++ src/app.module.ts
@@ -0,0 +1,39 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RabbitPublisherService } from './rabbit-publisher/rabbit-publisher.service';
+import { WorkersModule } from './worker/module/workers.module';
+// import { Employee } from './schemas/employee.entity';
+// import { env } from 'process';
+import { AuthzModule } from 'fintegrate-auth';
+
+import { TasksModule } from './tasks/module/tasks.module';
+import { TransformDataStructure } from './transformDataStructure/convertData';
+import { MessagesModule } from './message/module/messages.module';
+import { UserModule } from './user/module/users.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    UserModule,
+    TasksModule,
+    WorkersModule,
+    MessagesModule,
+    TasksModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule, WorkersModule, AuthzModule],
+      useFactory: async () => ({
+        uri: process.env.MONGODB_URI,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers:[AppController],
+  providers: [AppService, RabbitPublisherService, TransformDataStructure],
+})
+export class AppModule {}
