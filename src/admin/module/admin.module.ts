1fe1860815232230216d8573ebb0ffc4d2473dc6
Racheli <c0504197194@gmail.com>
2024-08-06 14:49:14 +0300

    add getUsersBusinessId to controller and fixing the function to return something true

--- src/admin/module/admin.module.ts
+++ src/admin/module/admin.module.ts
@@ -1,7 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from '../admin.controller';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { Employee, EmployeeSchema } from 'src/schemas/employee.entity';
+import { Department, DepartmentSchema } from 'src/schemas/department.entity';
+import {
+  Organization,
+  OrganizationSchema,
+} from 'src/schemas/organization.entity';
+import { Project, ProjectSchema } from 'src/schemas/project.entity';
+import { User, UserSchema } from 'src/schemas/user.entity';
+import { Role, RoleSchema } from 'src/schemas/EmployeeRole.entity';
+import { AdminService } from '../services/admin.service';
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Employee.name, schema: EmployeeSchema },
+      { name: Department.name, schema: DepartmentSchema },
+      { name: Organization.name, schema: OrganizationSchema },
+      { name: Project.name, schema: ProjectSchema },
+      { name: Role.name, schema: RoleSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
+  ],
+  providers: [AdminService],
   controllers: [AdminController],
 })
 export class AdminModule {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -11,16 +11,19 @@ import { AuthzModule } from 'fintegrate-auth';
 import { TasksModule } from './tasks/module/tasks.module';
 import { TransformDataStructure } from './transformDataStructure/convertData';
 import { MessagesModule } from './message/module/messages.module';
-
+// import { UserModule } from './user/module/users.module';
+import { AdminModule } from './admin/module/admin.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    // UserModule,
     TasksModule,
     WorkersModule,
     MessagesModule,
     TasksModule,
+    AdminModule,
     MongooseModule.forRootAsync({
       imports: [ConfigModule, WorkersModule, AuthzModule],
       useFactory: async (config: ConfigService) => ({
