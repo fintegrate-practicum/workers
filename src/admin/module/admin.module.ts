00094b46672fea750eb5da1b48ad2bdd2ef8ebb5
Racheli <c0504197194@gmail.com>
2024-08-07 09:48:04 +0300

    fix addGetClientsByBusinessId

--- src/admin/module/admin.module.ts
+++ src/admin/module/admin.module.ts
@@ -2,12 +2,6 @@ import { Module } from '@nestjs/common';
 import { AdminController } from '../admin.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Employee, EmployeeSchema } from 'src/schemas/employee.entity';
-import { Department, DepartmentSchema } from 'src/schemas/department.entity';
-import {
-  Organization,
-  OrganizationSchema,
-} from 'src/schemas/organization.entity';
-import { Project, ProjectSchema } from 'src/schemas/project.entity';
 import { User, UserSchema } from 'src/schemas/user.entity';
 import { Role, RoleSchema } from 'src/schemas/EmployeeRole.entity';
 import { AdminService } from '../services/admin.service';
@@ -15,9 +9,6 @@ import { AdminService } from '../services/admin.service';
   imports: [
     MongooseModule.forFeature([
       { name: Employee.name, schema: EmployeeSchema },
-      { name: Department.name, schema: DepartmentSchema },
-      { name: Organization.name, schema: OrganizationSchema },
-      { name: Project.name, schema: ProjectSchema },
       { name: Role.name, schema: RoleSchema },
       { name: User.name, schema: UserSchema },
     ]),
